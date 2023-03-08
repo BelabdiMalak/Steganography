c80fb8e80da88def55ae0f94b5cbcaf89a7fd5f1
malak <malakbelabdi@gmail.com>
2023-03-08 21:34:54 +0100

    encode message into image

--- main.py
+++ main.py
@@ -1,15 +1,35 @@
 import cv2 as cv
 import numpy as np
 
-# Read the image input 
-def ReadImage():
+
+def getImage():
     path  = input("Enter the path to your image please \n")
     image = cv.imread(path, 1)
     if image is None:
         print("Empty image path, I can't hide the message.")
         return 0
     else : 
-        print("Image well entered")
+        print("Your image is well entered.")
         return image
 
-ReadImage()
+
+def getMessage():
+    message = input("Enter a message to hide \n")
+    while(message == ''):
+        message = input("Enter a message to hide\n")
+    print("Your message is well entered.")
+    return message
+
+
+
+def encodeMessage():
+    message = getMessage()
+    image = getImage()
+    BinaryMessage = ''.join([ format(ord(i), "08b") for i in message ])
+    
+    # Compare if the image pixels are enough to cover the message
+    n_bytes = image.shape[0] * image.shape[1] * 3
+    if len(BinaryMessage) > n_bytes:
+        raise ValueError("[!] Insufficient bytes, need bigger image or less data.")
+
+encodeMessage()
